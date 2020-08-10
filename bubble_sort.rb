581c10d35eb93dcf7038f6bcd9d6e1875fb204a0
mateomh <mmojicah.84@gmail.com>
2020-08-10 11:22:04 -0500

    Fix linter errors

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,51 +1,47 @@
 def bubble_sort(var_array)
-  aux=0
-  iteration=var_array.length-1
-  while iteration>0
-    order_flag=true
+  iteration = var_array.length-1
+  while iteration > 0
+    order_flag = true
     for i in 0...iteration do
-      if var_array[i]>var_array[i+1]
-        order_flag=false
-        aux=var_array[i]
-        var_array[i]=var_array[i+1]
-        var_array[i+1]=aux
+      if var_array[i] > var_array[i+1]
+        order_flag = false
+        aux = var_array[i]
+        var_array[i] = var_array[i+1]
+        var_array[i+1] = aux
       end
     end
     if order_flag
       break
     end
-    iteration-=1
+    iteration -= 1
   end 
   return var_array
 end
 
 
 def bubble_sort_by(var_array)
-  aux=0
   iteration=var_array.length-1
-
-  while iteration>0
-    order_flag=true
+  while iteration > 0
+    order_flag = true
     for i in 0...iteration do
-
       if yield(var_array[i], var_array[i+1]) > 0
-        order_flag=false
-        aux=var_array[i]
-        var_array[i]=var_array[i+1]
-        var_array[i+1]=aux
+        order_flag = false
+        aux = var_array[i]
+        var_array[i] = var_array[i+1]
+        var_array[i+1] = aux
       end
     end
     if order_flag
       break
     end
-    iteration-=1
+    iteration -= 1
   end
 
   return var_array
 end
 
 
-numbers=[6,5,3,1,8,7,4,-1,-10,-100]
+numbers = [6,5,3,1,8,7,4,-1,-10,-100]
 print bubble_sort(numbers)
 print "\n"
 

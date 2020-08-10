7c58d7ca29d7f2c56f8c0a9bbfc7ae94fe2928ac
mateomh <mmojicah.84@gmail.com>
2020-08-10 11:48:44 -0500

    Change one line if for ternary operator and add next to the if statements

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,4 +1,5 @@
 def bubble_sort(var_array)
+
   iteration = var_array.length - 1
   while iteration.positive?
     order_flag = true
@@ -8,21 +9,20 @@ def bubble_sort(var_array)
         aux = var_array[i]
         var_array[i] = var_array[i + 1]
         var_array[i + 1] = aux
+      else
+        next
       end
     end
-    if order_flag
-      break
-    end
-
+    order_flag ? break : nil
     iteration -= 1
   end
 
   var_array
 end
 
-
 def bubble_sort_by(var_array)
-  iteration=var_array.length - 1
+
+  iteration = var_array.length - 1
   while iteration.positive?
     order_flag = true
     (0...iteration).each do |i|
@@ -31,24 +31,22 @@ def bubble_sort_by(var_array)
         aux = var_array[i]
         var_array[i] = var_array[i + 1]
         var_array[i + 1] = aux
+      else
+        next
       end
     end
-    if order_flag
-      break
-    end
-
+    order_flag ? break : nil
     iteration -= 1
   end
 
   var_array
 end
 
-
 numbers = [6, 5, 3, 1, 8, 7, 4, -1, -10, -100]
 print bubble_sort(numbers)
 print "\n"
 
-string_array = bubble_sort_by(['hi', 'hello', 'hey']) do |left, right|
+string_array = bubble_sort_by(%w(hi hello hey)) do |left, right|
   if left.is_a? String and right.is_a? String
     left.length - right.length
   elsif left.is_a? Numeric and right.is_a? Numeric
